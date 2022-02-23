f0e25af2170d57f128c7654eb2ea9e1e9c1f24bf
qusgusals <qusgusals12@naver.com>
2022-02-23 17:26:11 +0900

    Dodge
    프레임

---
+++ Dodge2/Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //이동에 사용할 리지드바디 컴포넌트
+    public Rigidbody playerRigidbody;
+    //이동 속력
+    public float speed = 8f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
